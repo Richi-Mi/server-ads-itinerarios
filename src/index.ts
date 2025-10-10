28146d34f84c660dd2b1eb08cb03d569dedf576b
Mendoza Castañeda José Ricardo <joseanakin@outlook.es>
2025-10-09 21:50:46 -0600

    AAHH maldito error

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-import { PostgresDataSource } from "./data/PostgresDataSource";
 import { Server } from "./presentation/Server";
 
 (async () => {
-  await PostgresDataSource.initialize()
-  console.log('Base de datos conectada');
-  
   Server.start()
 })()
--- src/presentation/Server.ts
+++ src/presentation/Server.ts
@@ -3,10 +3,25 @@ import Elysia from "elysia";
 import cors from '@elysiajs/cors'
 
 import { usuarioRoutes } from "./usuario";
+import { PostgresDataSource } from "../data/PostgresDataSource";
 
 export class Server {
     public static start() {
         const app = new Elysia()
+            .decorate('db', PostgresDataSource)
+            .onStart(async ({ decorator }) => {
+                try {
+                    console.log('Base de datos conectada');
+                    await decorator.db.initialize();
+                }
+                catch(error) {
+                    console.error('Error al conectar con la base de datos:', error);
+                    process.exit(1);
+                }
+            })
+            .onStop(async ({ decorator }) => {
+                await decorator.db.destroy();
+            })
             .use(cors())
             .get("/", () => "Server is running")
             .use(usuarioRoutes)
